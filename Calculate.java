0d7dbdfe529aa7501978238d51b17e97393e3d69
PankajSharmaIT <101395936+PankajSharmaIT@users.noreply.github.com>
2022-03-11 18:41:28 +0530

    Create Calculate.java

---
+++ Calculate.java
@@ -0,0 +1,10 @@
+public class Calculate
+{
+static int a =6;
+static int b =2;
+static int d;
+  public static void main(String[] args){
+ int d = a+b;    
+ system.out.println("Addition :"+ d);
+  }
+}
