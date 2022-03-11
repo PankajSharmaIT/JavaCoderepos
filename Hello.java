55ddccbe5aea1e9c8d80267beb04b9faadca08d5
PankajSharmaIT <101395936+PankajSharmaIT@users.noreply.github.com>
2022-03-11 17:17:16 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 
 public Class FirstProgram()
 {
-static String str = "Hello India";
+static String str = "Welcome to India";
 public static void main(String[] args){
 System.out.println(str);
+System.out.println("Please maintain the peace and prosperity of the country");
+
 }
 }
