27770f437ed5118a77258705c145764b18d3c3e4
PankajSharmaIT <101395936+PankajSharmaIT@users.noreply.github.com>
2022-03-11 16:01:01 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 
 public Class FirstProgram()
 {
-static String str = "Hello India";
+static String str = "Welcome";
 public static void main(String[] args){
 System.out.println(str);
+  for(int i=0;i<=10;i++){
+    System.out.println(str);
 }
 }
